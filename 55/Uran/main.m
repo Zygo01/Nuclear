97d917516f64e0c8db08a4c283ece0d222b49d9d
Zygo01 <kjagla0@gmail.com>
2022-05-14 14:42:31 +0200

    done Uran

--- 55/Uran/main.m
+++ 55/Uran/main.m
@@ -5,16 +5,19 @@ y = M(700:end,2);
 [pks, loc, extra] = findpeaks(y,'MinPeakHeight',75,"MinPeakDistance",150);
 loc = loc.+700;
 
-E = [184.36;240.18;293.17;349.70;606.3;764.76;1115.48;1232.94;1757.52];
+E = [186.2;240.0;295.2;351.9;609.3;768.4;1120.3;1238.1;1764.5];
 [p,s] = polyfit(loc, E, 1);
 dp=sqrt (diag (s.C)/s.df)*s.normr;
 xp=polyval(p,linspace(700, 8193, 7493));
 
 
-plot(xp,y);
+plot(loc,E,"r.","markersize", 12,
+     x,xp );
 axis("tight", "tic");
-axis([160, 1900, 0, 2300]);
+xlabel("chanel");
+ylabel("E [KeV]");
+axis([700, 8000, 0, 2000]);
 set(gcf, 'PaperSize', [23.2 8.2], 'PaperPosition', [0 0 23.2 8.2])
-set (gca (), "ytick", 0:200:2200, "xtick", 0:50:1900)
-print ("Plots/PeaksEnergyUnits.pdf");
+set (gca (), "ytick", 0:100:2000, "xtick", 700:200:8000)
+print ("Plots/EnergyPeaksPolyfit.pdf");
 #close();
